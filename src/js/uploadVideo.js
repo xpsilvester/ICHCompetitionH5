442815a297b58142de332aba11de009aa149025a
xpsilvester <wuxp5@mail2.sysu.edu.cn>
2018-12-10 20:57:47 +0800

    updatevideo

--- src/js/uploadVideo.js
+++ src/js/uploadVideo.js
@@ -1,17 +1,9 @@
 $(()=>{
     let swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+        slidesPerView: 'auto',
+        initialSlide : 1,
+        centeredSlides: true,
+        spaceBetween: 20,
+        grabCursor: true
     });
 })
