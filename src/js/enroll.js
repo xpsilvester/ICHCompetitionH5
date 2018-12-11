94cdde322c5b46190d8d70dbaae7dc744b7da6ca
runoob <test@runoob.com>
2018-12-11 16:42:52 +0800

    enroll test

--- src/js/enroll.js
+++ src/js/enroll.js
@@ -3,35 +3,59 @@ let phone = "";
 let code = "";
 let province = "";
 let city = "";
+let school = "";
 let grade = "";
 let teacher = "";
+let ok = false;
+
+let getVerifyCode = () => {
+    $(".code .yuko-textfield_textbtn").on("touchstart",function(){
+        console.log('tag', '');
+        let cellphone = $.trim($("#phone").val());
+        if(cellphone == ""){
+            alert("请填写手机号码");
+        }
+        else if(!/^(\+86)?1[0-9]{10}$|^[569][0-9]{7}$/.test(cellphone)){
+            alert("手机号码格式不正确");
+        }
+        else{
+            getVerifyCodeAjax(cellphone);
+        }
+    });
+}
+
+let changeCodeStyle = () => {
+    let time = 120;
+    $(".code .yuko-textfield_textbtn").unbind();
+    $(".code .yuko-textfield_textbtn").html("重新发送("+time+"s)");
+    $(".code .yuko-textfield_textbtn").css("color","#ccc");
+    let timer = setInterval(function(){
+        if(time == 0){
+            clearInterval(timer);
+            $(".code .yuko-textfield_textbtn").css("color","#fc0");
+            getVerifyCode();
+        }
+        else{
+            time--;
+            $(".code .yuko-textfield_textbtn").html("重新发送("+time+"s)");
+        }
+    },1000);
+}
 
 let initProvince = () => {
+    $(".province .yuko-select_items").html("");
     for(let i = 0;i < zone.length;i++){
         $(".province .yuko-select_items").append('<div>' + zone[i].name + '</div>');
     }
-    setTimeout(function(){
-        // $(".province .yuko-select_items").delegate('div', 'click', function(ev){
-        //     province = $(this).html();
-        //     console.log(111,$(this).html());
-        //     appendCity(province);
-        // });
-        $(".province .yuko-select_items>div").on("click",function(){
-            province = $(this).html();
-            console.log(111,$(this).html());
-            $(".city .yuko-select_items>div").unbind();
-            $(".city .yuko-select_items").html("");
-            appendCity(province);
-        });
-    },1000);
     
-    // $(".province .yuko-select_items>div").on("click",function(){
-    //     province = $(this).html();
-    //     console.log(111,$(this).html());
-    //     $(".city .yuko-select_items>div").unbind();
-    //     $(".city .yuko-select_items").html("");
-    //     appendCity(province);
-    // });
+    $(".province .yuko-select_items>div").on("touchstart",function(){
+        province = $(this).html();
+        $(".city .yuko-select_items>div").unbind();
+        $(".city .yuko-select_items").html("");
+        $(".city .yuko-select_selected").html("请选择");
+        city = "";
+        appendCity(province);
+    });
 }
 
 let appendCity = (province) => {
@@ -47,7 +71,96 @@ let appendCity = (province) => {
     });
 }
 
-$(()=>{
-    initProvince();
+let selectSchool = () => {
+    $(".school .yuko-textfield_searchbtn").on("touchstart",function(){
+        let word = $.trim($("#school").val());
+        if(word == ""){
+            alert("请填写学校");
+        }
+        else{
+            selectSchoolAjax(word);
+        }
+    });
+}
 
+let schoolChoice = (data) => {
+    $(".school .yuko-list_items").html("");
+    for(let i = 0;i < data.length;i++){
+        $(".school .yuko-list_items").append('<div>' + data[i] + '</div>');
+    }
+    $(".school .yuko-list_items").removeClass("yuko-list_hide");
+    $(".school .yuko-list_items>div").on("touchend",function(){
+        school = $(this).html();
+        $("#school").val(school);
+        $(".school .yuko-list_items").addClass("yuko-list_hide");
+    });
+}
+
+let selectGrade = () => {
+    $(".grade .yuko-select_items>div").on("touchstart",function(){
+        grade = $(this).html();
+    });
+}
+
+let confirmInfo = () => {
+    $(".confirm1 .yuko-radio").on("touchstart",function(){
+        ok = true;
+    });
+}
+
+let submit = () => {
+    $(".submit").on("touchstart",function(){
+        name = $.trim($("#name").val());
+        phone = $.trim($("#phone").val());
+        code = $.trim($("#code").val());
+        teacher = $.trim($("#teacher").val());
+        let flag = true;
+        if(name == ""){
+            alert("请填写孩子中文姓名");
+            flag = false;
+        }
+        if(phone == ""){
+            alert("请填写家长手机号码");
+            flag = false;
+        }
+        else if(!/^(\+86)?1[0-9]{10}$|^[569][0-9]{7}$/.test(phones)){
+            alert("手机号码格式不正确");
+            flag = false;
+        }
+        if(code == ""){
+            alert("请填写手机验证码");
+            flag = false;
+        }
+        if(province == ""){
+            alert("请选择省");
+            flag = false;
+        }
+        if(city == ""){
+            alert("请选择市");
+            flag = false;
+        }
+        if(school == ""){
+            alert("请选择学校");
+            flag = false;
+        }
+        if(grade == ""){
+            alert("请选择年级");
+            flag = false;
+        }
+        if(!ok){
+            alert("请确认信息属实");
+            flag = false;
+        }
+        if(flag){
+            submitAjax(name,phone,code,province,city,school,grade,teacher,ok);
+        }
+    });
+}
+
+$(()=>{
+    getVerifyCode();
+    initProvince();
+    selectSchool();
+    selectGrade();
+    confirmInfo();
 });
