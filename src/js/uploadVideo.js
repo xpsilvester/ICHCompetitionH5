eae4fcc0ad4e03d4dbc0090511a6e4cc6d843d3f
xpsilvester <xpsilvester@gmail.com>
2018-12-12 14:08:37 +0800

    add videostep1

--- src/js/uploadVideo.js
+++ src/js/uploadVideo.js
@@ -4,9 +4,19 @@ $(()=>{
         initialSlide : 1,
         centeredSlides: true,
         spaceBetween: 10,
-        grabCursor: true
+        grabCursor: true,
+        loop: true,
+        loopFillGroupWithBlank: true
     });
-    
+    //监听左右滑动
+    let moveX = 0;
+    $('.swiper-container').on('touchstart',function(){
+        moveX = swiper.getTranslate()
+    });
+    $('.swiper-container').on('touchend',function(){
+        (swiper.getTranslate()-moveX) != 0 ? $('.tips').css('opacity','0'):"";
+    });
+
     //播放视频
     $('.video_play').on('click',function(){
         $(this).parent().find('video').get(0).play();
