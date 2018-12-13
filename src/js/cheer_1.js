5de05e913a7eb3502c9201dc0bf789c3b8a75089
zhaoyangkanshijie <2191732408@qq.com>
2018-12-13 17:59:37 +0800

    cheer1

--- src/js/cheer_1.js
+++ src/js/cheer_1.js
@@ -1,3 +1,27 @@
+let getVideo = () => {
+    getVideoAjax();
+}
+
+let setVideo = (src) => {
+    $(".content .content-video").attr("src",src);
+}
+
+let videoPlay = () => {
+    $(".content-play").on("touchstart",function(){
+        $(this).addClass("not-display");
+        $(".content-video").get(0).play();
+    });
+}
+
+let videoPause = () => {
+    $(".content-video").on("touchstart",function(){
+        if($(".content-play").hasClass("not-display")){
+            $(".content-play").removeClass("not-display");
+            $(".content-video").get(0).pause();
+        }
+    });
+}
+
 $(()=>{
     
 });
