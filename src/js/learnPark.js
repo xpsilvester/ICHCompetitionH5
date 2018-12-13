5d111971c377e9699c9e27131003ae6eab8b74b9
RyougiChan <ryougi.chan.fire@gmail.com>
2018-12-13 15:23:05 +0800

    Add learnPark

--- src/js/learnPark.js
+++ src/js/learnPark.js
@@ -1,3 +1,28 @@
-$(()=>{
-    console.log(1)
-})
+$(() => {
+    'use strict';
+
+    $('#yuko-videoplay').on('touchend', function (evt) {
+
+        let tar = evt.target,
+            $videoMainCon = $('#yuko-videoplay'),
+            $video = $('#yuko-videoplay-video video');
+
+        if (tar.id === 'yuko-videoplay-icon' || tar.nodeName.toLocaleLowerCase() === 'video') {
+            if (!$videoMainCon.hasClass('playing')) {
+                $videoMainCon.addClass('playing');
+                $video[0].play();
+            } else {
+                $videoMainCon.removeClass('playing');
+                $video[0].pause();
+            }
+        }
+    });
+
+    $('#yuko-videodesc').on('touchend', function () {
+        if (!$(this).hasClass('expanded')) {
+            $(this).addClass('expanded');
+        } else {
+            $(this).removeClass('expanded');
+        }
+    });
+});
