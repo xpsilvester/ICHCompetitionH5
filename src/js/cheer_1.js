6da90cbd961303f7495115fdc9a7f85b3426e4e3
zhaoyangkanshijie <2191732408@qq.com>
2018-12-14 08:59:11 +0800

    cheer1

--- src/js/cheer_1.js
+++ src/js/cheer_1.js
@@ -1,27 +1,57 @@
+let getNumber = () => {
+    getNumberAjax();
+}
+
+let setNumber = (number) => {
+    $(".header-number").html(number);
+}
+
 let getVideo = () => {
     getVideoAjax();
 }
 
 let setVideo = (src) => {
-    $(".content .content-video").attr("src",src);
+    $(".screen-video").attr("src",src);
 }
 
 let videoPlay = () => {
-    $(".content-play").on("touchstart",function(){
+    $(".screen-play").on("touchstart",function(){
         $(this).addClass("not-display");
-        $(".content-video").get(0).play();
+        $(".screen-video").get(0).play();
     });
 }
 
 let videoPause = () => {
-    $(".content-video").on("touchstart",function(){
-        if($(".content-play").hasClass("not-display")){
-            $(".content-play").removeClass("not-display");
+    $(".screen-video").on("touchstart",function(){
+        if($(".screen-play").hasClass("not-display")){
+            $(".screen-play").removeClass("not-display");
             $(".content-video").get(0).pause();
         }
     });
 }
 
+let getFavor = () => {
+    getFavorAjax();
+}
+
+let setFavor = (isFavor) => {
+    if(isFavor){
+        $(".main-heart").addClass("favor");
+    }
+}
+
+let favor = () => {
+    $(".main-heart").on("click",function(){
+        $(".main-heart").addClass("favor");
+        favorAjax();
+    });
+}
+
 $(()=>{
-    
+    getNumber();
+    getVideo();
+    videoPlay();
+    videoPause();
+    getFavor();
+    favor();
 });
